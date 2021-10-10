f56f8c937b1cfbd4b7571404c7025dbdf68b3eab
santycano <santy.cano@hotmail.com>
2021-10-10 12:08:19 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express"),
+    app = express(),
+    http = require("http"),
+    server = http.createServer(app),
+    mongoose = require("mongoose");
+
+
+var router = express.Router();
+
+router.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.use(router);
+
+mongoose.connect("mongodb://localhost/tvshows", function (err, res) {
+  if (err) {
+    console.log("ERROR: connecting to Database. " + err);
+  }
+  app.listen(3000, function () {
+    console.log("Node server running on http://localhost:3000");
+  });
+});
